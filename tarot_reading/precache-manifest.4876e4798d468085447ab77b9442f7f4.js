300983ce2a1a16326c839fe9753020c4fb116b0e
ppereztirador <40394931+ppereztirador@users.noreply.github.com>
2020-04-05 13:04:33 +0100

    Tarot reading app rough version

---
+++ tarot_reading/precache-manifest.4876e4798d468085447ab77b9442f7f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aec6eb7168087a10b76fd9b7f061a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1939137db193603df9e7",
+    "url": "/static/js/2.05b876da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05b876da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a424358b37222580fb5f",
+    "url": "/static/js/main.8e56d21a.chunk.js"
+  },
+  {
+    "revision": "3473b56409672a718883",
+    "url": "/static/js/runtime-main.30aa9002.js"
+  }
+]);
